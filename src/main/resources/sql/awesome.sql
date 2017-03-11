4913da9d849d4f57593c47d09dc9b45c4b070e52
adam <654316301@qq.com>
2017-03-11 12:10:41 +0800

    sql变更

--- src/main/resources/sql/awesome.sql
+++ src/main/resources/sql/awesome.sql
@@ -10,10 +10,35 @@ Target Server Type    : MYSQL
 Target Server Version : 50632
 File Encoding         : 65001
 
-Date: 2017-03-09 13:48:05
+Date: 2017-03-11 12:05:22
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for course
+-- ----------------------------
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE `course` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(255) DEFAULT NULL COMMENT '课程名',
+  `code` varchar(32) DEFAULT NULL COMMENT '课程代号',
+  `profession` varchar(32) DEFAULT NULL COMMENT '所属专业',
+  `remark` varchar(255) DEFAULT NULL COMMENT '课程描述',
+  `content` text COMMENT '课程内容',
+  `credit` int(11) DEFAULT NULL COMMENT '学分',
+  `status` int(1) DEFAULT '1' COMMENT '状态 0禁用1启用',
+  `create_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `video_url` varchar(255) DEFAULT NULL COMMENT '视频路径',
+  `video_type` int(1) DEFAULT NULL COMMENT '视频类型（1、本地上传视频 2、在线视频）',
+  `teacher` int(11) DEFAULT NULL COMMENT '所属教师',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of course
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for resource
@@ -47,8 +72,8 @@ CREATE TABLE `role` (
 -- ----------------------------
 -- Records of role
 -- ----------------------------
-INSERT INTO `role` VALUES ('1', 'admin');
-INSERT INTO `role` VALUES ('2', 'customer');
+INSERT INTO `role` VALUES ('1', '教师');
+INSERT INTO `role` VALUES ('2', '学生');
 
 -- ----------------------------
 -- Table structure for role_resource
@@ -74,21 +99,45 @@ INSERT INTO `role_resource` VALUES ('2', '1');
 INSERT INTO `role_resource` VALUES ('2', '3');
 
 -- ----------------------------
+-- Table structure for student_course
+-- ----------------------------
+DROP TABLE IF EXISTS `student_course`;
+CREATE TABLE `student_course` (
+  `student_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `is_collect` int(1) DEFAULT '0' COMMENT '是否收藏 1是 0 否',
+  `is_join` int(1) DEFAULT '0' COMMENT '是否参加 1是 0否',
+  UNIQUE KEY `index_student_course` (`student_id`,`course_id`) USING BTREE COMMENT '唯一索引'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of student_course
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) DEFAULT NULL,
+  `uid` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `username` varchar(32) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
+  `name` varchar(32) DEFAULT NULL COMMENT '姓名',
+  `mobile` varchar(32) DEFAULT NULL COMMENT '手机号',
+  `office_phone` varchar(32) DEFAULT NULL COMMENT '座机号',
+  `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
+  `depart` varchar(32) DEFAULT NULL COMMENT '部门',
+  `major` varchar(32) DEFAULT NULL COMMENT '专业',
+  `sex` int(1) DEFAULT '0' COMMENT '姓名 男0女1',
+  `status` int(1) DEFAULT '1' COMMENT '状态 0禁用 1启用',
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'root', '123');
-INSERT INTO `user` VALUES ('2', 'admin', '222');
+INSERT INTO `user` VALUES ('1', 'root', '123', null, null, null, null, null, null, '0', '1');
+INSERT INTO `user` VALUES ('2', 'admin', '222', null, null, null, null, null, null, '0', '1');
 
 -- ----------------------------
 -- Table structure for user_role
